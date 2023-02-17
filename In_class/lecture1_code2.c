01e96d1bc0b39b8e4672d6fb8394fb29d2f0ef77
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2023-02-17 23:10:03 +0530

    Add files via upload

---
+++ In_class/lecture1_code2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	short int  a = 32770;
+	printf("%d", sizeof(a));
+	
+	printf("\n%d", sizeof(int));
+	printf("\n%d", sizeof(short int));
+	printf("\n%d", sizeof(long int));
+	printf("\n%d", sizeof(long long int));
+	printf("\n%d", sizeof(long));
+	printf("\n%i", a);
+	printf("\n%u", -100);
+	printf("\n%d", -100);
+	
+	
+}
