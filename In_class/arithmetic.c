ea9cd9d8b2e8847e7916e0f5d9748a541982a116
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2023-02-21 23:40:31 +0530

    Add files via upload

---
+++ In_class/arithmetic.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+//int main(){
+//	int i = 3;
+//	int l = i/-2;
+//	int k = i%-2;
+//	printf("%d %d\n",l,k);
+//	return 0;
+//}
+//int main()
+//	int x  = 5.3 % 2;
+//	int x = 5 + 5 == 10;
+//	printf("Value of x is %d",x);
+//	return 0;
+//}
+int main(){
+	printf("     *");
+	printf("\n    ***");
+	printf("\n   *****");
+	printf("\n  *******");
+	printf("\n *********");
+//	printf("\n******");
+}
+
+
