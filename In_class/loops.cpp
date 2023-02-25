bbf5e6f037fb3ae071da79389a4feec7bdd11909
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2023-02-25 23:10:25 +0530

    Add files via upload

---
+++ In_class/loops.cpp
@@ -0,0 +1,28 @@
+//#include <stdio.h>
+//int main() {
+//	int n = 1 ;
+//	while (n<=10){
+//		printf("%d \n", n);
+//		n++ ;
+//		
+//	}
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+//{
+//	int n = 1;
+//	do 
+//	{
+//		printf("hello world \n");
+//		n++;
+//	}while(n<=10);
+//	
+//	
+//}
+//for(int n = 1; n<=1; n++){
+//	
+//}
+
+
