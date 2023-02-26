ad19a5a7d50a2d50ecc3e5df4c38fd0e5759b087
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2023-02-26 23:57:22 +0530

    Add files via upload

---
+++ In_class/nested_if_else.cpp
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+int main()
+{   int x = 1;
+    if (x == 1)
+        if (x == 0)
+            printf("inside if\n");
+        else
+            printf("inside else if\n");
+    else
+        printf("inside else\n");
+        return 0;
+    }
+
