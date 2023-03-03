555939c29f259981ffaf5d6580c6514cbe61a71b
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2023-03-03 23:38:05 +0530

    Add files via upload

---
+++ In_class/sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int n1,n2,sum;
+	printf("Enter First  integer: ");
+	
+	scanf("%d", &n1); 
+	
+	printf("Enter  Second integer: ");
+	scanf("%d",&n2);
+	sum = n1 + n2;
+	printf("%d",sum);
+	
+}
