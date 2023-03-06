377e49c997ea62510c1a76084f3e0406f3e16bab
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2023-03-06 22:46:03 +0530

    Add files via upload

---
+++ In_class/bitwise_opr.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main() 
+
+
+//{
+//int a=8,b=3;
+//printf("%d",a||b);
+//return 0;
+//}
+
+
+
+//{
+//int a=8,b=3;
+//printf("%d",a^b);
+//return 0;
+//}
+
+//{
+//
+//int a=56,b=27;
+//printf("%d",a|b);
+//return 0;
+//}
+
+{
+int a=10;
+printf("%d",~a);
+//-(n+1) is the formula we use here to solve the compliment of any number
+return 0;
+}
+
+
+
