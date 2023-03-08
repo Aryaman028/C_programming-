d773b83c8a6850be7bfa71a8374020431d6ba8dc
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2023-03-08 20:57:49 +0530

    Add files via upload

---
+++ In_class/ternary_operator.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int m,n ;
+    scanf("%d %d", &m,&n);
+    (m >n) ? printf("%d",m) : printf("%d",n);
+//    using ternary operator for finding the maxing value between 2 numbers it works like
+//	value = experssion1 ? experssion2 : expersion3 ;
+//	if expersion is true then expersion2 will be e cuted if false then expression3 will be excuted
+    
+    
+    
+    
+
+      
+    return 0;
+}
+
