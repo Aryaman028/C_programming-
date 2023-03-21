0b119ddae269e80a8c86e5a26e2879c2947a3753
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2023-03-21 23:28:58 +0530

    Add files via upload

---
+++ In_class/CallingFunc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+// Create a function
+void myFunction() {
+  printf("Hey there!");
+}
+
+int main() {
+  myFunction(); // call the function
+  return 0;
+}
+
+// Output : "Hey there!"
