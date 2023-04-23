1ae64093a06198f41fa4e5bebc07d32b0d6c2058
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2023-04-23 23:51:46 +0530

    Add files via upload

---
+++ In_class/ArrQues.c
@@ -0,0 +1,25 @@
+// Program to find the average of n numbers using arrays
+
+#include <stdio.h>
+
+int main() {
+
+  int marks[10], i, n, sum = 0;
+  double average;
+
+  printf("Enter number of elements: ");
+  scanf("%d", &n);
+
+  for(i=0; i < n; ++i) {
+    printf("Enter number%d: ",i+1);
+    scanf("%d", &marks[i]);
+          
+    sum += marks[i];
+  }
+
+  average = (double) sum / n;
+
+  printf("Average = %.2lf", average);
+
+  return 0;
+}
