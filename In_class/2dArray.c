b319c2906dcc0f71504fab858d79be7daa5e3ed1
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2023-04-28 22:17:29 +0530

    Add files via upload

---
+++ In_class/2dArray.c
@@ -0,0 +1,25 @@
+// 2d array storing and displaying values
+#include <stdio.h>
+
+const int CITY = 2;
+const int WEEK = 7;
+
+int main()
+{
+    int temperature[CITY][WEEK];
+    for (int i = 0; i < CITY; ++i) {
+        for(int j = 0; j < WEEK; ++j) {
+            printf("City %d, Day %d: ", i+1, j+1);
+            scanf("%d", &temperature[i][j]);
+        }
+    }
+
+    printf("\nDisplaying values: \n\n");
+    for (int i = 0; i < CITY; ++i) {
+        for(int j = 0; j < WEEK; ++j)
+        {
+            printf("City %d, Day %d = %d\n", i+1, j+1, temperature[i][j]);
+        }
+    }
+    return 0;
+}
