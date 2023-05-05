071ae0c8e9793905ebbf5126b3a95e2bc6848d3b
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2023-05-05 23:22:32 +0530

    Add files via upload

---
+++ In_class/patternwithloop.c
@@ -0,0 +1,31 @@
+/*
+pattern printing in c using for loop  
+3 3 3 3 3    
+3 2 2 2 3    
+3 2 1 2 3    
+3 2 2 2 3    
+3 3 3 3 3   
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int len = 2*n - 1;
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len; j++) {
+            int min = i < j ? i : j;
+            min = min < len-i ? min : len-i-1;
+            min = min < len-j-1 ? min : len-j-1;
+            printf("%d ", n-min);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
